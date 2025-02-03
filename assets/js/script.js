8f41d4dd7143bd3382f1b7bf79ee3696bb359128
William Italia <williamitalia70@outlook.com>
2025-02-03 03:56:38 -0300

    feat: remove and add items, filter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,39 @@
 const boxTheme = document.querySelector('.box-theme');
 const body = document.body;
+const filter = document.querySelector('.filter-input');
+const clear = document.querySelector('#clear');
+const msg = document.querySelector('.msg');
+const list = document.querySelector('.list-items');
+const items = list.getElementsByTagName('li');
+const clearFilter = document.querySelector('.clear-filter');
+const form = document.querySelector('#form');
+const itemInput = document.querySelector('#item-input');
+
+let timeOutId;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const item = itemInput.value;
+
+    if(item.trim() != '') {
+        showMsg('Item adicionado com sucesso!', 'ok');
+        
+        list.prepend(createItem(item));
+        itemInput.value = '';
+        
+        itemInput.focus();
+        
+        verifiedFilter();
+        verifiedList();
+        clearFil();
+    } else {
+        showMsg('Não foi possivel adicionar', 'erro');
+    }
+});
+
+
+// dark mode
 boxTheme.addEventListener('click', (e) => {
     body.classList.toggle('dark');
 
@@ -8,7 +42,130 @@ boxTheme.addEventListener('click', (e) => {
     } else {
         boxTheme.innerHTML = `<img src="./assets/img/moon.png" alt="">`;
     };
+    
 });
+
+list.addEventListener('click', (e) => {
+    if(e.target && e.target.closest('button').classList.contains('remove-item')) {
+        const listItem = e.target.closest('li');
+
+        listItem ? listItem.remove() : null;    
+        showMsg('Item apagado com sucesso!', 'ok');
+
+    }
+    
+    
+    
+    clearFil();
+    verifiedList();
+    verifiedFilter();
+});
+
+// clear
+clear.addEventListener('click', () => {
+    list.replaceChildren();
+
+    showMsg('Todos os items foram apagados com sucesso!!!', 'ok');
+    verifiedList();
+});
+
+// filtro
+filter.addEventListener('input', () => {
+    const filterText = filter.value.toLowerCase();
+
+    Array.from(items).forEach(item => {
+        const itemText = item.textContent.toLowerCase();
+
+        if (itemText.includes(filterText)) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+
+    verifiedFilter();
+});
+
+
+// limpeza do filtro
+clearFilter.addEventListener('click', (e) => {
+    
+    clearFil();
+    verifiedFilter();
+});
+
+
+function createText(item) {
+    const text = document.createTextNode(item);
+    return text;
+}
+
+
+function createItem(item) {
+
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+
+    btn.classList.add('remove-item');
+    btn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+
+    li.appendChild(createText(item));
+    li.appendChild(btn);
+
+    return li;
+
+}
+
+function clearFil() {
+    filter.value = '';
+        
+    Array.from(items).forEach(item => {
+        item.style.display = 'flex';
+    });
+
+}
+
+function showMsg(content, type) {
+
+    msg.textContent = content;
+    msg.classList.add(type);
+
+    msg.classList.add('active');
+
+    if(timeOutId) {
+        clearTimeout(timeOutId);
+    }
+    
+    timeOutId = setTimeout(() => {
+        msg.classList.remove('active');
+        setTimeout(() => {
+            msg.classList.remove(type);
+        }, 500);
+    }, 2000);
+}
+
+function verifiedList() {
+    if (list.children.length > 1) {
+        clear.style.display = 'block';
+        filter.style.display = 'block';
+    } else {
+        clear.style.display = 'none';
+        filter.style.display = 'none';
+    }
+}
+
+
+
+function verifiedFilter() {
+    if (filter.value !== '' && list.children.length > 1) {
+        clearFilter.style.display = 'block';
+    } else {
+        clearFilter.style.display = 'none';
+    }
+}
+
+verifiedList();
+verifiedFilter(filter);
 
 
 // document.addEventListener('DOMContentLoaded', () => {
